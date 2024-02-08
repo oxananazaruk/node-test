ef4e32fe26ca8fc000e40e076daa6770259e831e
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-02-08 21:40:08 +0200

    test

---
+++ isLeapYear.js
@@ -0,0 +1,21 @@
+const isLeapYear = (year) => {
+    if (year === undefined) {
+      throw new Error('year must exist')  
+    }
+
+    if (typeof year !== "number") {
+        throw new Error('year must be number')
+    }
+    if (!Number.isInteger(year)) {
+        throw new Error('year must be integer')
+    }
+
+    if (year < 42) {
+        throw new Error('year must 42 or more');
+    }
+    const date = new Date(year, 2, 0);
+    const days = date.getDate();
+    return (29 === days)
+}
+
+module.exports = isLeapYear;
